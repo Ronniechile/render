e5d2c03519f006664a7d3a948e220a7423868a7f
Ronnie Sasmay <ronnie@MacBook-Air.local>
2024-04-14 18:06:23 -0400

    tercera modificacion

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 import express from 'express'
+import pg from 'pg'
+import {config} from 'dotenv'
+
 
 const app = express()
+const pool= new pg.Pool({
+connectionString: process.env.DATABASE_URL,
+ssl:true
+})
 
 app.get('/',(req,res)=> {
     res.send('HolaMundo Atari')
 })
 
+app.get('/ping', async(req,res)=> {
+ const result = await pool.query('SELECT NOW()')
+ return res.json(result.rows[0])
+})
 
 
 app.listen(3000)
